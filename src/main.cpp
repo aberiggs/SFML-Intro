174013a579d68de9f1e7264eeb2dc232988b6308
Sprocket Riggs <sprocket710@gmail.com>
2024-03-27 00:50:49 -0400

    Basic movement of an object

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <SFML/Graphics.hpp>
 
+void CheckAndMove(sf::Shape& object) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            // left key is pressed: move our character 
+            object.move(-1.f, 0.f);
+        }
+
+         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            // left key is pressed: move our character 
+            object.move(1.f, 0.f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            // left key is pressed: move our character 
+            object.move(0.f, -1.f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            // left key is pressed: move our character 
+            object.move(0.f, 1.f);
+        }
+} 
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML-Intro");
-    sf::RectangleShape shape(sf::Vector2f(100.f, 100.f));
-    shape.setFillColor(sf::Color::Red);
+    sf::RectangleShape shape(sf::Vector2f(40.f, 40.f));
+    shape.setFillColor(sf::Color(190, 200, 220, 255));
 
     while (window.isOpen())
     {
@@ -15,7 +37,10 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        CheckAndMove(shape);
+
+
+        window.clear(sf::Color(45, 50, 50, 255));
         window.draw(shape);
         window.display();
     }
